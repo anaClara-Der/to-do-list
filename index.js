d16f94d77b72774d13ef6f4cd15c5feae166c548
Ana Clara <anaclaraderario@gmail.com>
2020-11-24 16:37:18 -0300

    Se borraron const que ya no se usaban y se acomodó el codigo

--- index.js
+++ index.js
@@ -1,73 +1,76 @@
-const fecha = document.querySelector("#fecha"); 
+const fecha = document.querySelector("#fecha");
 let texterea = document.querySelector("#texterea");
-let botonListado = document.querySelector(".boton-listado");
 const form = document.getElementById("listado");
 let tareasHacerUl = document.querySelector(".tareas-hacer-ul");
-let hacerTareas = []
-let arrayDeTareas = []
-let color = document.querySelector("#color"); //opciones de color para agregar a las notas
+let hacerTareas = [];
+let color = document.querySelector("#color"); 
 const template = document.getElementById("tareas-template").content;
 const fragmen = document.createDocumentFragment();
- 
 
-
-
-// Funcionamiento
+// Escribir la tarea
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    tarjetaTareas();
-   
-})
+  e.preventDefault();
+  tarjetaTareas();
+});
 
-//Las tareas escritas se pasan al otro div
-function tarjetaTareas(){
-    hacerTareas = []
-    if (texterea.value) {
-        hacerTareas.push(texterea.value);
-        arrayDeTareas.push(texterea.value);
-        hacerTareas.forEach(item => {
-            const clone = template.cloneNode(true);
-            clone.querySelector("p").textContent = item;
-            clone.querySelector(".tareasDecoracion").style.backgroundColor = color.value;
-            clone.querySelector(".circulo").addEventListener("click", function (){
-                this.classList.toggle("hecho")
-            })
-            clone.querySelector(".borrar").addEventListener("click", function(){
-                this.parentElement.remove()
-            })
-            fragmen.appendChild(clone);
-        })
-        tareasHacerUl.appendChild(fragmen);
-    }
-    form.reset()
-}
-
-
-//FECHA 
-function tituloFecha () {
-    const fechas = new Date();
-    const dias = ["Domingo" , "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]; 
-    const meses = ["Enero" , "Febrero" , "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]; 
-    return fecha.innerHTML = "Hoy es "+ dias[fechas.getDay()] + " " + fechas.getDate() + " de " + meses[fechas.getMonth()];
-}
-
+//Fecha
 tituloFecha();
 
-
-/*
-//FUNCIONES
-//marcar el círculo en verde si se hizo
-function chequeado() {
-    let check = document.querySelector(".circulo");
-    check.addEventListener("click", function () {
+//Fuciones
+//Las tareas escritas se pasan al otro div
+function tarjetaTareas() {
+  hacerTareas = [];
+  if (texterea.value) {
+    hacerTareas.push(texterea.value);
+    hacerTareas.forEach((item) => {
+      const clone = template.cloneNode(true);
+      clone.querySelector("p").textContent = item;
+      clone.querySelector(".tareasDecoracion").style.backgroundColor =
+        color.value;
+      clone.querySelector(".circulo").addEventListener("click", function () {
         this.classList.toggle("hecho");
-    })
+      });
+      clone.querySelector(".borrar").addEventListener("click", function () {
+        this.parentElement.remove();
+      });
+      fragmen.appendChild(clone);
+    });
+    tareasHacerUl.appendChild(fragmen);
+  }
+  form.reset();
 }
-//Al hacer click en el tachito se borra 
-function eliminar (){
-    let borrar = document.querySelector(".delete");
-    borrar.addEventListener("click", function(){
-        borrar.parentElement.remove();
-    })
+//Fecha cabezera
+function tituloFecha() {
+  const fechas = new Date();
+  const dias = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+    "Domingo",
+  ];
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  return (fecha.innerHTML =
+    "Hoy es " +
+    dias[fechas.getDay()] +
+    " " +
+    fechas.getDate() +
+    " de " +
+    meses[fechas.getMonth()]);
 }
-*/

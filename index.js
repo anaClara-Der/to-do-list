aaba9a032106c3f2d1b00289eaee50a9cab7a780
Ana Clara <anaclaraderario@gmail.com>
2020-11-24 16:27:12 -0300

    Se modificó el codigo de la fecha. Se usó templates para construir la lista de tareas

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
-let fecha = document.querySelector("#fecha"); 
-let tareas = document.querySelector(".tarea");
+const fecha = document.querySelector("#fecha"); 
+let texterea = document.querySelector("#texterea");
 let botonListado = document.querySelector(".boton-listado");
-let tareasHacer = document.querySelector(".tareas-hacer-ul");
-let tareasGuardar; 
+const form = document.getElementById("listado");
+let tareasHacerUl = document.querySelector(".tareas-hacer-ul");
+let hacerTareas = []
+let arrayDeTareas = []
 let color = document.querySelector("#color"); //opciones de color para agregar a las notas
+const template = document.getElementById("tareas-template").content;
+const fragmen = document.createDocumentFragment();
  
 
-//Título: Fecha
-let fechas = new Date(); 
-var dia= new Array(7);
-dia[0]="Domingo";
-dia[1]="Lunes";
-dia[2]="Martes";
-dia[3]="Miercoles";
-dia[4]="Jueves";
-dia[5]="Viernes";
-dia[6]="Sabado";
-//para el mes
-var fechasMes = fechas.getMonth() + 1;
-var mesok = (fechasMes < 10) ? '0' + fechasMes : fechasMes;
-var mesok=new Array(12);
-mesok[0]="Enero";
-mesok[1]="Febrero";
-mesok[2]="Marzo";
-mesok[3]="Abril";
-mesok[4]="Mayo";
-mesok[5]="Junio";
-mesok[6]="Julio";
-mesok[7]="Agosto";
-mesok[8]="Septiembre";
-mesok[9]="Octubre";
-mesok[10]="Noviembre";
-mesok[11]="Diciembre";
-//Fecha de título
-fecha.innerHTML = "Hoy es "+ dia[fechas.getDay()] + " " + fechas.getDate() + " de " + mesok[fechas.getMonth()];
+
 
 // Funcionamiento
-botonListado.addEventListener("click", function () {
-    tareasGuardar = tareas.value; 
-    colores = color.value; 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     tarjetaTareas();
-    chequeado();
-    eliminar(); 
+   
 })
-localStorage.removeItem("tarea" , tareasHacer)
+
+//Las tareas escritas se pasan al otro div
+function tarjetaTareas(){
+    hacerTareas = []
+    if (texterea.value) {
+        hacerTareas.push(texterea.value);
+        arrayDeTareas.push(texterea.value);
+        hacerTareas.forEach(item => {
+            const clone = template.cloneNode(true);
+            clone.querySelector("p").textContent = item;
+            clone.querySelector(".tareasDecoracion").style.backgroundColor = color.value;
+            clone.querySelector(".circulo").addEventListener("click", function (){
+                this.classList.toggle("hecho")
+            })
+            clone.querySelector(".borrar").addEventListener("click", function(){
+                this.parentElement.remove()
+            })
+            fragmen.appendChild(clone);
+        })
+        tareasHacerUl.appendChild(fragmen);
+    }
+    form.reset()
+}
 
 
+//FECHA 
+function tituloFecha () {
+    const fechas = new Date();
+    const dias = ["Domingo" , "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]; 
+    const meses = ["Enero" , "Febrero" , "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]; 
+    return fecha.innerHTML = "Hoy es "+ dias[fechas.getDay()] + " " + fechas.getDate() + " de " + meses[fechas.getMonth()];
+}
+
+tituloFecha();
+
+
+/*
 //FUNCIONES
 //marcar el círculo en verde si se hizo
 function chequeado() {
     let check = document.querySelector(".circulo");
-    check.classList.add("cursorMano")
     check.addEventListener("click", function () {
         this.classList.toggle("hecho");
     })
@@ -58,17 +66,8 @@ function chequeado() {
 //Al hacer click en el tachito se borra 
 function eliminar (){
     let borrar = document.querySelector(".delete");
-    borrar.classList.add("cursorMano")
     borrar.addEventListener("click", function(){
         borrar.parentElement.remove();
     })
 }
-
-function tarjetaTareas(){
-    if (tareasGuardar) { 
-        tareasHacer.insertAdjacentHTML("afterbegin", `<div class="tareasDecoracion"><i class=" circulo far fa-circle "></i> <li> ${tareasGuardar} </li>
-        <i class="far fa-trash-alt delete"></i> </div>`);
-        document.querySelector(".tareasDecoracion").style.backgroundColor = colores; 
-    }
-}
-
+*/
